f4f07b2f52cbce60f0cdade0dd96c8fe8d710b02
Yoga Arta G <grahanantyo@gmail.com>
2022-06-15 17:07:39 +0700

    make it better

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,13 @@
 const psql = require("pg");
 const { Pool } = psql;
 
-const { DB_USER, DB_HOST, DB_DATABASE, DB_PASS, DB_PORT} = process.env;
+// const { DB_USER, DB_HOST, DB_DATABASE, DB_PASS, DB_PORT} = process.env;
 
 const db = new Pool({
-    user: DB_USER,
-    host: DB_HOST,
-    database: DB_DATABASE,
-    password: DB_PASS,
-    port: DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 module.exports = db;
